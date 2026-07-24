2f5fe5933c1b5cda0f1d2190ea72c8d157d773ca
Jaeyong Lee <jaeyong1.lee@gmail.com>
2026-07-25 05:19:28 +0900

    chore: clean up diagnostic test scripts after 100% test pass

--- test_import.mjs
+++
@@ -1,7 +0,0 @@
-try {
-  console.log('Testing import of server/index.js...');
-  const module = await import('./server/index.js');
-  console.log('Successfully imported server/index.js! Export keys:', Object.keys(module));
-} catch (err) {
-  console.error('Import error:', err);
-}
