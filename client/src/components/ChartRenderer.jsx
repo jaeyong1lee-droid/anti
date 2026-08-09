4fe7a4c899ad4acc6f43c34ec882f53eaa145f87
Jaeyong Lee <jaeyong1.lee@gmail.com>
2026-08-09 09:08:42 +0900

    style(chart): 차트 하단 여백 40으로 재조정

--- client/src/components/ChartRenderer.jsx
+++ client/src/components/ChartRenderer.jsx
@@ -161,7 +161,7 @@ const ChartRenderer = ({ data }) => {
       <div className="p-4 w-full h-[320px] sm:h-[400px]">
         {/* Recharts Container */}
         <ResponsiveContainer width="100%" height="100%">
-          <LineChart data={chartData} margin={{ top: 5, right: 10, left: 10, bottom: 30 }}>
+          <LineChart data={chartData} margin={{ top: 5, right: 10, left: 10, bottom: 40 }}>
             <CartesianGrid strokeDasharray="3 3" stroke="#334155" opacity={0.5} />
             <XAxis 
               dataKey="x" 
